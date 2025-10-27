f707d42497858a91858f3af4e351a17fc21211f2
JarningGau <jarninggau@gmail.com>
2025-10-27 13:21:54 +0800

    simplify export interfaces

--- darlin/__init__.py
+++ darlin/__init__.py
@@ -3,6 +3,7 @@ DARLIN Python - Python implementation of CARLIN sequence analysis tools
 
 Main features:
 - CRISPR-Cas9 sequence alignment
+- Multiple amplicon templates support (Col1a1, Rosa, Tigre)
 - Allele calling
 - Mutation annotation
 - Advanced sequence analysis API
@@ -14,29 +15,16 @@ __version__ = "0.1.0"
 __author__ = "Jarning Gau"
 
 # Export main API interfaces
-from .api import analyze_sequences, quick_analyze, AnalysisResult
-
-# Export core functionality modules
-from .alignment.cas9_align import cas9_align, nt2int, int2nt
-from .mutations import Mutation, MutationType, annotate_mutations
-
-# Export configuration
-from .config.amplicon_configs import ORIGINAL_CARLIN
+from .api import analyze_sequences
+from .config.amplicon_configs import AmpliconConfig
+from .utils.build_config import build_carlin_config
 
 __all__ = [
     '__version__',
     '__author__',
     # Main API
     'analyze_sequences',
-    'quick_analyze', 
-    'AnalysisResult',
-    # Core functionality
-    'cas9_align',
-    'nt2int',
-    'int2nt',
-    'Mutation',
-    'MutationType',
-    'annotate_mutations',
-    # Configuration
-    'ORIGINAL_CARLIN'
+    # Create new template config
+    'AmpliconConfig',
+    'build_carlin_config'
 ] 
